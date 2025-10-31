a4578e30107aaec13a59e05349e31a6b722d0270
pedro <pedrodiaz0487@gmail.com>
2025-10-31 19:32:43 -0300

    subir

--- src/caracteristicas-personales/caracteristicas-personales.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CaracteristicasPersonalesService } from './caracteristicas-personales.service';
-import { CaracteristicasPersonalesController } from './caracteristicas-personales.controller';
-
-@Module({
-  controllers: [CaracteristicasPersonalesController],
-  providers: [CaracteristicasPersonalesService]
-})
-export class CaracteristicasPersonalesModule {}
--- src/caracteristicas-personales/dto/update-caracteristicas-personale.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCaracteristicasPersonaleDto } from './create-caracteristicas-personale.dto';
-
-export class UpdateCaracteristicasPersonaleDto extends PartialType(CreateCaracteristicasPersonaleDto) {}
