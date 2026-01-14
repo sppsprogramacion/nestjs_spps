6dff2f55e8900c5049d15dac12f8a5fb8333543e
pedro <pedrodiaz0487@gmail.com>
2026-01-14 13:43:08 -0300

    subir

--- src/entradas-salidas/entradas-salidas.controller.ts
+++ src/entradas-salidas/entradas-salidas.controller.ts
@@ -1,34 +1,93 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ParseIntPipe, NotFoundException, Put } from '@nestjs/common';
 import { EntradasSalidasService } from './entradas-salidas.service';
+import { Auth, GetUser } from 'src/auth/decorators';
+import { Usuario } from 'src/usuario/entities/usuario.entity';
 import { CreateEntradasSalidaDto } from './dto/create-entradas-salida.dto';
-import { UpdateEntradasSalidaDto } from './dto/update-entradas-salida.dto';
+import { DateValidationPipe } from 'src/pipes/date-validation.pipe';
+import { TimeValidationPipe } from 'src/pipes/time-validation.pipe';
+import { UpdateEntradaPrincipalEgresoDto } from './dto/update-entrada-principal-egreso.dto';
+import { UpdateEntradaSalidasCancelarDto } from './dto/update-entradas-salidas-cancelar.dto';
 
 @Controller('entradas-salidas')
 export class EntradasSalidasController {
   constructor(private readonly entradasSalidasService: EntradasSalidasService) {}
 
   @Post()
-  create(@Body() createEntradasSalidaDto: CreateEntradasSalidaDto) {
-    return this.entradasSalidasService.create(createEntradasSalidaDto);
-  }
+  @Auth()
+  create(
+    @GetUser("usuario") user: Usuario, //decorador  personalizado obtiene Usuario de la ruta donde esta autenticado
+    @Body() data: CreateEntradasSalidaDto
+  ) {
+    
+    return this.entradasSalidasService.create(data, user);
+  }  
 
-  @Get()
+  @Get('todos')
   findAll() {
     return this.entradasSalidasService.findAll();
   }
 
+  //BUSCAR  XID CIUDADANO
+  @Get('lista-xciudadano')  
+  async findXCiudadano(
+    @Query('id_ciudadano', ParseIntPipe) id_ciudadano: string    
+  ) {    
+    
+    return this.entradasSalidasService.findXCiudadano(+id_ciudadano);
+  }
+  //FIN BUSCAR  XID CIUDADANO....................................................
+
+  //BUSCAR PENDIENTES SALIDA - fecha de ingreso actual - segun organismo del usuario, los que aun no registran.. 
+  //..hora de salida
+  @Get('lista-pendientes-salida')  
+  @Auth()
+  async findPendientesSalida(
+    @GetUser("usuario") user: Usuario, //decorador  personalizado obtiene Usuario de la ruta donde esta autenticado
+  ) {        
+
+    return this.entradasSalidasService.findPendientesSalidaFechaActual(user);
+  }
+  //FIN BUSCAR  PENDIENTES SALIDA....................................................
+  
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseIntPipe) id: string) {    
+    
     return this.entradasSalidasService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateEntradasSalidaDto: UpdateEntradasSalidaDto) {
-    return this.entradasSalidasService.update(+id, updateEntradasSalidaDto);
+  //PARA RUTA NO DEFINIDA
+  @Get('*')
+  rutasNoDefinidas() {
+    throw new NotFoundException('No se encontró la ruta especificada. Verifique si la ruta es correcta');
   }
+  //FIN PARA RUTA NO DEFINIDA...........
+  
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.entradasSalidasService.remove(+id);
+  //EGRESO REGISTRO
+  @Put('egreso')
+  @Auth()
+  updateEgreso(
+    @GetUser("usuario") user: Usuario, //decorador  personalizado obtiene Usuario de la ruta donde esta autenticado
+    @Query('id_registro', ParseIntPipe) id_registro: string ,
+    @Body() dataDto: UpdateEntradaPrincipalEgresoDto
+  ) {
+
+    
+    return this.entradasSalidasService.registrarEgreso(+id_registro, dataDto, user);
   }
+  //FIN EGRESO REGISTRO.................................
+
+  //CANCELAR REGISTRO
+  @Put('cancelar')
+  @Auth()
+  updateAnular(
+    @GetUser("usuario") user: Usuario, //decorador  personalizado obtiene Usuario de la ruta donde esta autenticado
+    @Query('id_registro', ParseIntPipe) id_registro: string ,
+    @Body() dataDto: UpdateEntradaSalidasCancelarDto
+  ) {
+    
+
+    return this.entradasSalidasService.cancelarRegistro(+id_registro, dataDto, user);
+  }
+  //FIN CANCELAR REGISTRO.................................
 }
