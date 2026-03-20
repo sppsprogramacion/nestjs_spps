9f43aa8591a2163ca6d75c6195a35532c7bb1d27
pedro <pedrodiaz0487@gmail.com>
2026-03-20 06:40:20 -0300

    subir

--- src/historial-procesal/historial-procesal.module.ts
+++ src/historial-procesal/historial-procesal.module.ts
@@ -1,13 +1,15 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { HistorialProcesalService } from './historial-procesal.service';
 import { HistorialProcesalController } from './historial-procesal.controller';
 import { AuthModule } from 'src/auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { HistorialProcesal } from './entities/historial-procesal.entity';
+import { IngresosInternoModule } from 'src/ingresos-interno/ingresos-interno.module';
 
 @Module({
   imports: [
     AuthModule,
+    forwardRef(() => IngresosInternoModule),
     TypeOrmModule.forFeature([
       HistorialProcesal
     ])
