bf4d9ed0ff790a3eb10483328ae1518698622851
Pedro <TuCorreo@example.com>
2026-08-11 14:01:19 -0300

    subir

--- src/huellas-cambios/entities/huellas-cambio.entity.ts
+++ src/huellas-cambios/entities/huellas-cambio.entity.ts
@@ -1,1 +1,29 @@
-export class HuellasCambio {}
+import { Column, PrimaryGeneratedColumn } from "typeorm";
+
+
+export class HuellaCambio {
+
+    @PrimaryGeneratedColumn({
+        type: 'bigint',
+        unsigned: true
+    })
+    version: string;
+
+    @Column({
+        type: 'bigint',
+        unsigned: true
+    })
+    huella_id: string;
+
+    @Column({
+        type: 'enum',
+        enum: ['ALTA', 'BAJA']
+    })
+    accion: string;
+
+    @Column({
+        type: 'datetime',
+        default: () => 'CURRENT_TIMESTAMP'
+    })
+    fecha: Date;
+}
